bb13d10bf08f9097f899777381d9c69ed4861965
halyna-baklanova <novabalka@gmail.com>
2024-11-15 15:17:05 +0200

    solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,44 +1,91 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
     GenreList,
     GenreDetail,
-    movie_list,
-    movie_detail,
     ActorList,
     ActorDetail,
+    CinemaHallViewSet,
+    MovieViewSet
+)
+
+router = routers.DefaultRouter()
+
+router.register("movie", MovieViewSet)
+
+cinemahall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create",
+    }
+)
+
+cinemahall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
+
+movie_list = MovieViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create",
+    }
+)
+
+movie_detail = MovieViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
 )
 
 urlpatterns = [
     path(
         "genres/",
         GenreList.as_view(),
-        name="genre-list"
+        name="genre_list"
     ),
     path(
         "genres/<int:pk>/",
         GenreDetail.as_view(),
-        name="genre-detail"
+        name="genre_detail"
     ),
-path(
+    path(
         "actors/",
         ActorList.as_view(),
-        name="actor-list"
+        name="actor_list"
     ),
     path(
         "actor/<int:pk>/",
         ActorDetail.as_view(),
-        name="actor-detail"
+        name="actor_detail"
+    ),
+    path(
+        "cinemahalls/",
+        cinemahall_list,
+        name="cinemahall_list"
+    ),
+    path(
+        "cinemahall/<int:pk>/",
+        cinemahall_detail,
+        name="cinemahall_detail"
     ),
     path(
         "movies/",
-        movie_list,
-        name="movie-list"
+        include(router.urls),
+        name="movie_list"
     ),
     path(
         "movies/<int:pk>/",
-        movie_detail,
-        name="movie-detail"
+        include(router.urls),
+        name="movie_detail"
     ),
 ]
 
